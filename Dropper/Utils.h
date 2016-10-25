be2051295e1bc05e7c84cf7a33c2f2b1c3cdb694
adde9708 <adde9708@unseen.is>
2016-10-25 12:53:28 +0200

    utils.h

--- Dropper/Utils.h
+++ Dropper/Utils.h
@@ -19,12 +19,18 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
 
-#include "define.h"
-#include "EncodingUtils.h"
 #include "CodeBlock.h"
+#include "EncodingUtils.h"
+#include "define.h"
 
-INT32 SharedMapViewOfSection(HANDLE hHandle, SIZE_T iSectionSize, PHANDLE pSectionHandle, PVOID *pBaseAddr1, PVOID *pBaseAddr2);
-void CopySegmentIntoSections(PVOID *pProcessSection, PVOID pModuleSection, INT32 *pSectionPointer, PSECTION_SEGEMENT_INFO sSegment, PVOID pSegmentContent, UINT32 iSegmentSize);
-INT32 GetRandomModuleName(GENERAL_INFO_BLOCK *sInfoBlock, LPCWSTR szDebugLibraryName);
+INT32 SharedMapViewOfSection(HANDLE hHandle, SIZE_T iSectionSize,
+                             PHANDLE pSectionHandle, PVOID *pBaseAddr1,
+                             PVOID *pBaseAddr2);
+void CopySegmentIntoSections(PVOID *pProcessSection, PVOID pModuleSection,
+                             INT32 *pSectionPointer,
+                             PSECTION_SEGEMENT_INFO sSegment,
+                             PVOID pSegmentContent, UINT32 iSegmentSize);
+INT32 GetRandomModuleName(GENERAL_INFO_BLOCK *sInfoBlock,
+                          LPCWSTR szDebugLibraryName);
 
 #endif
